660945369f4275cadc472f232fc50f8a971aa27a
Andrea Vincenzo Abbondanza <andreabbondanza.developer@outlook.com>
2018-02-01 13:04:44 +0100

    First release

---
+++ DewXamarinFiles/DewXamarinFiles/DewXamarinFiles.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace DewCore.Xamarin.Files
+{
+    /// <summary>
+    /// Dew Xamarin Files Class
+    /// </summary>
+    public static class DewXamarinFiles
+    {
+        /// <summary>
+        /// Local settings name (without extension), default: __dew_loc_sett
+        /// </summary>
+        public static string SettingsName = "__dew_loc_sett";
+        /// <summary>
+        /// Default buffer size for file write/read
+        /// </summary>
+        public static int BufferSize = 1024;
+        /// <summary>
+        /// Read a file as byte array (if app, pass DewXamarinFiles.ApplicationPath() as basepath)
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public async static Task<byte[]> ReadLocalFileAsync(string path)
+        {
+            byte[] result = null;
+            using (Stream s = File.Open(path, FileMode.Open))
+            {
+                result = new byte[s.Length];
+                var offset = 0;
+                while (await s.ReadAsync(result, offset, BufferSize) > 0)
+                {
+                    offset += BufferSize;
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// Read a file as string (if app, pass DewXamarinFiles.ApplicationPath() as basepath)
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public async static Task<string> ReadLocalFileTextAsync(string path)
+        {
+            string result = null;
+            using (Stream s = File.Open(path, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(s))
+                {
+                    result = await sr.ReadToEndAsync();
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// Write a file as byte array (if app, pass DewXamarinFiles.ApplicationPath() as basepath)
+        /// </summary>
+        /// <param name="s">data</param>
+        /// <param name="name"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public async static Task WriteLocalFileAsync(byte[] s, string name, string path = null)
+        {
+            using (FileStream file = File.Create(path + Path.DirectorySeparatorChar + name))
+            {
+                var offset = 0;
+                while (offset <= s.Length)
+                {
+                    await file.WriteAsync(s, BufferSize, offset);
+                    offset += BufferSize;
+                }
+            }
+        }
+        /// <summary>
+        /// Write a file as string (if app, pass DewXamarinFiles.ApplicationPath() as basepath)
+        /// </summary>
+        /// <param name="s">data</param>
+        /// <param name="name"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public async static Task WriteLocalFileTextAsync(string s, string name, string path = null)
+        {
+            using (FileStream file = File.Create(path + Path.DirectorySeparatorChar + name))
+            {
+                using (StreamWriter sw = new StreamWriter(file))
+                {
+                    await sw.WriteAsync(s);
+                }
+            }
+        }
+        /// <summary>
+        /// Return the base path for the current platform
+        /// </summary>
+        /// <returns></returns>
+        public static string ApplicationPath()
+        {
+            var assembly = Application.Current.GetType().Assembly;
+            string osv = "";
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                case Device.macOS:
+                    {
+                        osv = "Library";
+                        break;
+                    }
+                case Device.Android:
+                    break;
+                case Device.UWP:
+                case Device.WinRT:
+                case Device.WinPhone:
+                    {
+                        osv = "LocalState";
+                        break;
+                    }
+            }
+            return Path.Combine(Path.GetDirectoryName(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)), osv);
+
+        }
+        /// <summary>
+        /// Check if a file exists
+        /// </summary>
+        /// <param name="file">the path to the file</param>
+        /// <returns></returns>
+        public static bool CheckFileExists(string file)
+        {
+            var fil = new FileInfo(file);
+            return fil.Exists;
+        }
+        /// <summary>
+        /// Check if a directory exists
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static bool CheckDirectoryExists(string path)
+        {
+            var dir = new DirectoryInfo(path);
+            return dir.Exists;
+        }
+        /// <summary>
+        /// Write a local setting
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="option"></param>
+        /// <returns></returns>
+        public async static Task WriteLocalSetting(string key, string option)
+        {
+            var path = ApplicationPath();
+            if (!CheckFileExists(path + Path.DirectorySeparatorChar + SettingsName + ".json"))
+            {
+                await WriteLocalFileTextAsync("{}", SettingsName + ".json", path + Path.DirectorySeparatorChar);
+            }
+            string json = await ReadLocalFileTextAsync(path + Path.DirectorySeparatorChar + SettingsName + ".json");
+            Dictionary<string, string> settings = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            if (settings.ContainsKey(key))
+            {
+                string serializate = option;
+                settings[key] = serializate;
+            }
+            else
+                settings.Add(key, option.ToString());
+            await WriteLocalFileTextAsync(Newtonsoft.Json.JsonConvert.SerializeObject(settings), SettingsName + ".json", path + Path.DirectorySeparatorChar);
+        }
+        /// <summary>
+        /// Write a local setting of type T (T must be serializable)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="option"></param>
+        /// <returns></returns>
+        public async static Task WriteLocalSetting<T>(string key, T option)
+        {
+            var path = ApplicationPath();
+            if (!CheckFileExists(path + Path.DirectorySeparatorChar + SettingsName + ".json"))
+            {
+                await WriteLocalFileTextAsync("{}", SettingsName + ".json", path + Path.DirectorySeparatorChar);
+            }
+            string json = await ReadLocalFileTextAsync(path + Path.DirectorySeparatorChar + SettingsName + ".json");
+            Dictionary<string, string> settings = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            if (settings.ContainsKey(key))
+            {
+                string serializate = SerializeObject(option);
+                settings[key] = serializate;
+            }
+            else
+                settings.Add(key, option.ToString());
+            await WriteLocalFileTextAsync(Newtonsoft.Json.JsonConvert.SerializeObject(settings), SettingsName + ".json", path + Path.DirectorySeparatorChar);
+        }
+        /// <summary>
+        /// Check if a local setting exists
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public async static Task<bool> CheckLocalSettingExists(string key)
+        {
+            var path = ApplicationPath();
+            if (!CheckFileExists(Path.Combine(path, SettingsName + ".json")))
+            {
+                await WriteLocalFileTextAsync("{}", SettingsName + ".json", path + Path.DirectorySeparatorChar);
+            }
+            string json = await ReadLocalFileTextAsync(Path.Combine(ApplicationPath(), SettingsName + ".json"));
+            Dictionary<string, string> settings = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            return settings.ContainsKey(key);
+        }
+        /// <summary>
+        /// Read a local setting of type T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public async static Task<T> ReadLocalSetting<T>(string key)
+        {
+            if (!CheckFileExists(Path.Combine(ApplicationPath(), SettingsName + ".json")))
+            {
+                throw new FileNotFoundException();
+            }
+            string json = await ReadLocalFileTextAsync(Path.Combine(ApplicationPath(), SettingsName + ".json"));
+            Dictionary<string, string> settings = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            var deserialized = DeserializeObject<T>(settings[key]);
+            return deserialized;
+        }
+        /// <summary>
+        /// Read a local setting as string
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public async static Task<string> ReadLocalSetting(string key)
+        {
+            if (!CheckFileExists(Path.Combine(ApplicationPath(), SettingsName + ".json")))
+            {
+                throw new FileNotFoundException();
+            }
+            string json = await ReadLocalFileTextAsync(Path.Combine(ApplicationPath(), SettingsName + ".json"));
+            Dictionary<string, string> settings = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            return settings[key];
+        }
+        /// <summary>
+        /// Serialize to json an object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="toSerialize"></param>
+        /// <returns></returns>
+        public static string SerializeObject<T>(T toSerialize)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(toSerialize);
+        }
+        /// <summary>
+        /// Serialize to json an object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="toDeserialize"></param>
+        /// <returns></returns>
+        public static T DeserializeObject<T>(string toDeserialize)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(toDeserialize);
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,118 @@
 # DewXamarinFiles
-A library for xamarin forms to manage files and local settings
+A library for xamarin forms to manage files and local settings.
+
+## How to use
+
+This is a static class, so you can use the methods and attributes directly.
+
+### Files
+
+Here the methods/attributes that help you to manage files.
+
+#### Attributes
+
+- __BufferSize__: _int_ - Is the buffer size for read/write files
+
+#### Methods
+
+- __ReadLocalFileAsync__: (__path__: _string_ : _The path to the file_) : _return_ __byte[]__
+- __ReadLocalFileTextAsync__: (__path__: _string_ : _The path to the file_) : _return_ __string__
+- __WriteLocalFileAsync__: (_s_: _byte[]_ : The data to write, __name__: _string_ : _filename_, __path__: _string_ : _the path to the folder_)
+- __WriteLocalFileAsync__: (_s_: _string_ : The data to write, __name__: _string_ : _filename_, __path__: _string_ : _the path to the folder_)
+- __ApplicationPath__: _return_ __string__ : _the path to the local app folder with write permissions__
+- __CheckFileExists__: (__file__: _string_ : __file path__) : _return_ __bool__
+- __CheckDirectoryExists__: (__path__: _string_ : _directory path_) _return_ __bool__
+
+#### Example
+
+```csharp
+using DewCore.Xamarin.Files;
+using DewCore.Extensions.Strings;
+public class Example
+{
+    public async Task CreateFileWithText(string toWrite)
+    {
+        var basePath = DewXamarinFiles.ApplicationPath();
+        if(!DewXamarinFiles.CheckFileExists(basePath+Path.DirectorySeparator+"textfile.txt"))
+            await DewXamarinFiles.WriteLocalFileTextAsync(towrite,"textfile.txt",basePath);
+        if(!DewXamarinFiles.CheckFileExists(basePath+Path.DirectorySeparator+"bytefile"))
+            await DewXamarinFiles.WriteLocalFileTextAsync(towrite.ToBytes(),"bytefile",basePath);
+        string text = await DewXamarinFiles.ReadLocalFileTextAsync(basePath+Path.DirectorySeparator+"textfile.txt");
+        byte[] bytes = await DewXamarinFiles.ReadLocalFileAsync(basePath+Path.DirectorySeparator+"textfile.txt");
+    }
+}
+```
+#### NOTE
+In app environemnt, you need to pass always the _basePath_ because you don't have the permissions to write wherever you want. The methods supports absolute paths just for the future, in the eventually of MacOS, Linux, PC xamarin forms supports.
+
+### Local Settings
+
+Here the methods/attributes that help you to manage local settings.
+
+#### Attributes
+
+- __SettingsName__: _string_ - Is the local settings file name (without extension), default value is "_\_\_dew\_loc\_set_"
+
+#### Methods
+
+- __WriteLocalSettings__: (__string__: _key_ : _local setting key_, __string__: _option_ : _local setting value_)
+- __WriteLocalSettings\<T\>__: (__string__: _key_ : _local setting's key_, __T__: _option_ : _local setting value_) - T must implmenent a ToString method to serialize
+- __CheckLocalSettingExists__: (__string__: _key_ : _local setting's key_) : _return_ __bool__
+- __ReadLocalSetting__: (__string__: _key_ : _local setting's key_): _return_ __string__
+- __ReadLocalSetting\<T\>__: (__string__: _key_ : _local setting's key_): _return_ __T__
+- __SerializeObject\<T\>__: (__T__ toSerialize : _Object to serialize_): _return_ __string__
+- __DeserializeObject\<T\>__: (__string__ toDeserialize : _Object to deserialize_): _return_ __T__
+
+
+#### Example
+
+```csharp
+using DewCore.Xamarin.Files;
+using DewCore.Extensions.Strings;
+public class Example
+{
+    public class User
+    {
+        public string Name {get;set;}
+        public string Surname {get;set;}
+    }
+//-----------------------------------------------------------------------
+    public async Task<bool> WriteToken()
+    {
+        if(!DewXamarinFiles.CheckLocalSettingExists("token"))
+            return false;
+        var token = await DewXamarinFiles.ReadLocalSettings("token");
+        if(token.Length < 10)
+            while(token.Length < 10)
+                token += "0";
+        await DewXamarinFiles.WriteLocalSetting("token",token);
+        return true;
+    }
+//-----------------------------------------------------------------------
+    public async Task WriteUser(User u)
+    {
+        await DewXamarinFiles.WriteLocalSetting<User>("user",u);    
+    }
+//-----------------------------------------------------------------------
+    public async Task<User> ReadUser()
+    {
+        if(!DewXamarinFiles.CheckLocalSettingExists("user"))
+            return null;
+        return DewXamarinFiles.ReadLocalSetting<User>("user");
+    }
+//-----------------------------------------------------------------------
+}
+```
+
+### Note
+
+- Type __T__ must be serializable
+- When you try to read a local setting and the file doesn't exist, it will throw a __FileNotFoundException__, so Use _CheckLocalSettingExists_ before
+- When you call __CheckLocalSettings__, if the file doesn't exist it will be created with "{}" object into.
+- When you call a __Write__ function, if the file doesn't exist it will be created with "{}" object into.
+
+## About
+[Andrea Vincenzo Abbondanza](http://www.andrewdev.eu)
+
+## Donate
+[Help me to grow up, if you want](https://payPal.me/andreabbondanza)
